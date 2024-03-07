bc937ef433baddc042566534db0cd445e4e8e40c
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2024-03-07 16:25:41 +0530

    Espro Music

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EsproUpdate")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EsproSupport")
+SUPPORT_CHANNEL = int(getenv("SUPPORT_CHANNEL", "https://t.me/EsproUpdate"))
+SUPPORT_CHAT = int(getenv("SUPPORT_CHAT", "https://t.me/EsproSupport"))
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -86,9 +86,9 @@ clean = {}
 
 autoclean = []
 
-START_IMG_URL = getenv(
+START_IMG_URL = int(getenv(
     "START_IMG_URL", "https://telegra.ph/file/4f98411fc494e0491472e.jpg"
-)
+))
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/4f98411fc494e0491472e.jpg"
 )
